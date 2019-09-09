ff1b53d724dda1b32dbb484c4af0c8f9c1515247
Idowu Emehinola <hydeenoble39@gmail.com>
2019-09-09 15:04:27 +0100

    Finalized implemetation

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # csv-normalizer
+
+## How to Build and Run
+* Clone the Repo
+```
+git clone https://github.com/hydeenoble/csv-normalizer.git
+```
+* Install Dependecies
+```
+npm install
+```
+* Run application
+```
+node app.js /path/to/csv/file
+```
+
+## Assumptionps on the input file
+* Input CSV is UTF-8
+* First row contains column headers
+* Columns are always in same order as follows: Timestamp, Address, ZIP, FullName, FooDuration, BarDuration, TotalDuration, Notes
---
+++ app.js
@@ -0,0 +1,44 @@
+let converter = require("json-2-csv");
+let Helper = require("./helper");
+let readline = require("readline");
+
+let helper = new Helper();
+let stdinData = "";
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on("line", line => {
+    stdinData += line + "\n";
+}).on("close", () => {
+    let converterStdinData = converter.csv2jsonAsync(helper.normalizeUTF8(stdinData));
+    converterStdinData
+        .then(rows => {
+            rows.forEach((row, index) => {
+                try {
+                    row.Timestamp = helper.timeStampConverter(row.Timestamp);
+                    row.ZIP = helper.zipCodeFormatter(row.ZIP);
+                    row.FullName = helper.fullNameFormatter(row.FullName);
+                    row.FooDuration = helper.timeConverter(row.FooDuration);
+                    row.BarDuration = helper.timeConverter(row.BarDuration);
+                    row.TotalDuration = parseFloat(row.FooDuration) + parseFloat(row.BarDuration);
+                    row.Notes = row.Notes;
+                    row.Address = row.Address;
+                } catch (error) {
+                    console.log("Warning:", error.message + "on row " + (index + 1));
+                    rows.splice(index, 1);
+                }
+            });
+            return rows;
+        })
+        .then(jsonData => {
+            return converter.json2csvAsync(jsonData);
+        })
+        .then(csvData => {
+            console.log(csvData);
+        })
+        .catch(err => console.log("ERROR: " + err.message));
+});
