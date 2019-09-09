f2131fad1ed8782b92b2f83971a0e0f1469ebef1
Idowu Emehinola <hydeenoble39@gmail.com>
2019-09-09 15:24:16 +0100

    Added comments to helper methods

--- helper.js
+++ helper.js
@@ -1,20 +1,34 @@
 let moment = require("moment-timezone");
 class Helper {
+
+    /**
+     * 
+     * @param {Number} zipCode
+     * All ZIP codes should be formatted as 5 digits. If there are less than 5 digits, assume 0 as the prefix.
+     */
     zipCodeFormatter(zipCode) {
         let zipCodeString = zipCode.toString();
         if (zipCodeString.length < 5) {
             return zipCodeString.padStart(5, "0");
         } 
-        
         return zipCode;
-        
     }
 
+    /**
+     * 
+     * @param {String} fullName 
+     * All name columns should be converted to uppercase. There will be non-English names.
+     */
     fullNameFormatter(fullName) {
         return fullName.toUpperCase();
     }
-
-    /**Convertime Given time in HH:MS:SS.MS to second floating format */
+    
+    /**
+     * 
+     * @param {String} time 
+     * The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
+     * format (where MS is milliseconds); please convert them to a floating point seconds format.
+     */
     timeConverter(time) {
         try {
             let timeArray = time.split(":");
@@ -28,13 +42,23 @@ class Helper {
         }
     }
     
-    
-    /*replace invalid UTF-8 character with the Unicode Replacement Character*/
+    /**
+     * 
+     * @param {String} str 
+     * Replace invalid UTF-8 character with the Unicode Replacement Character
+     */
     normalizeUTF8(str) {
         const regx = /(?!([\u{0000}-\u{007F}]|[\u{0080}-\u{07FF}]|[\u{0800}-\u{FFFF}]|[\u{10000}-\u{10FFFF}]))/gu;
         return str.replace(regx, "\ufffd");
     }
 
+    /**
+     * 
+     * @param {String} timestamp 
+     * The Timestamp column should be formatted in ISO-8601 format.
+     * The Timestamp column should be assumed to be in US/Pacific time; 
+     * please convert it to US/Eastern.
+     */
     timeStampConverter(timestamp) {
         let convertTimeStamp = moment.tz(
             timestamp,
